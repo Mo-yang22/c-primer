875a029d02e82720bfe395f90740d0f4f376112f
skp <1123302861@qq.com>
2023-06-23 10:37:29 +0800

    update

--- RB_TREE/main.cpp
+++ RB_TREE/main.cpp
@@ -1,26 +1,32 @@
+/*
+ * @Author: skp 1123302861@qq.com
+ * @Date: 2023-06-22 20:45:56
+ * @LastEditors: skp 1123302861@qq.com
+ * @LastEditTime: 2023-06-23 10:17:20
+ * @FilePath: /c-primer/RB_TREE/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
+ * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 #include "RB_tree.cpp"
-int main()
-{
-    RBTree<int> rb;
+int main() {
+  RBTree<int> rb;
 
-    int arr[] = {10, 7, 8, 15, 5, 6, 11, 13, 12};
-    int n = sizeof(arr) / sizeof(int);
-    for (int i = 0; i < n; i++)
-    {
-        rb.Insert(arr[i]);
-    }
-    rb.HorizontalPrint();
-    int arr2[] = {2,4,22,21,24,23,27,26};
-    for(auto i :arr2)
-    {
-        rb.Insert(i);
-    }
-    rb.HorizontalPrint();
-    cout<<"...................."<<endl;
-    rb.Remove(27);
-    rb.Remove(8);
-    rb.Remove(6);
-    rb.HorizontalPrint();
-    return 0;
+  int arr[] = {10, 7, 8, 15, 5, 6, 11, 13, 12};
+  int n = sizeof(arr) / sizeof(int);
+  for (int i = 0; i < n; i++) {
+    rb.Insert(arr[i]);
+  }
+  rb.HorizontalPrint();
+  int arr2[] = {2, 4, 22, 21, 24, 23, 27, 26};
+  for (auto i : arr2) {
+    rb.Insert(i);
+  }
+  rb.HorizontalPrint();
+  cout << "...................." << endl;
+  rb.Remove(27);
+  rb.Remove(8);
+  rb.Remove(6);
+  rb.HorizontalPrint();
+  return 0;
 }
--- single.cpp
+++ single.cpp
@@ -1,43 +1,46 @@
+/*
+ * @Author: skp 1123302861@qq.com
+ * @Date: 2023-06-22 20:45:56
+ * @LastEditors: skp 1123302861@qq.com
+ * @LastEditTime: 2023-06-23 09:26:41
+ * @FilePath: /c-primer/single.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <iostream>
 #include <pthread.h>
 using namespace std;
-class single{
+class single {
 public:
-    static single* getinstacnce();
+  static single *getinstacnce();
+
 private:
-    //私有静态指针变量指向唯一实例
-    static single* p;
-    //静态锁,是因为静态成员函数只能操作静态变量
-    static pthread_mutex_t lock;
-    //私有化构造函数
-    single(){
-        pthread_mutex_init(&lock,NULL);
-    }
-    ~single();
+  //私有静态指针变量指向唯一实例
+  static single *p;
+  //静态锁,是因为静态成员函数只能操作静态变量
+  static pthread_mutex_t lock;
+  //私有化构造函数
+  single() { pthread_mutex_init(&lock, NULL); }
+  ~single();
 };
 //必须要声明一下吗?
 pthread_mutex_t single::lock;
 
-single* single::p=NULL;
+single *single::p = NULL;
 
-single* single::getinstacnce()
-{
-    if(p==NULL)
-    {
-        pthread_mutex_lock(&lock);
-        if(p==NULL)
-        {
-            p= new single;
-            cout<<"new"<<endl;
-        }
-        pthread_mutex_unlock(&lock);
+single *single::getinstacnce() {
+  if (p == NULL) {
+    pthread_mutex_lock(&lock);
+    if (p == NULL) {
+      p = new single;
+      cout << "new" << endl;
     }
-    return p;
+    pthread_mutex_unlock(&lock);
+  }
+  return p;
 }
-int main()
-{
-    single* s=single::getinstacnce(); 
-    single* s1=single::getinstacnce();
-    cout<<s<<endl<<s1<<endl;
-    return 0;
+int main() {
+  single *s = single::getinstacnce();
+  single *s1 = single::getinstacnce();
+  cout << s << endl << s1 << endl;
+  return 0;
 }
